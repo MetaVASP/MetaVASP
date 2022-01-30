1c8a62ad6c523e1b5cd4177f358db6e65cea3ec7
Pulkit Gupta <76155456+pulkit-30@users.noreply.github.com>
2022-01-30 23:30:12 +0530

    added pre-commit hooks

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-
 /**
  * Getting '/' request
  * response send @String `greeting`
@@ -8,13 +7,11 @@ const app = express();
 app.get("/", () => {
   res.send("Welcome to MetaVASP community site");
 });
-
 /**
  * Declaring port
  * Development port @ 8080
  */
 const port = process.env.PORT || 8080;
-
 // App listening @port
 app.listen(port, () => {
   console.log("Application Running @", port);
